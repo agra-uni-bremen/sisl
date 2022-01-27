cb2296894ca2154dbefcc24c349993238fdb6a8e
Sören Tempel <tempel@uni-bremen.de>
2022-01-27 12:07:13 +0100

    Add more test cases

--- tests/run.scm
+++ tests/run.scm
@@ -7,6 +7,21 @@
 
 (test-group "input format"
   (test-format "concrete input format"
-    #(#("c1" 8 #(42)))
+    #(#("c1" 16 #(23 42)))
     (make-input-format
-      (make-field "c1" 8 #u8(42)))))
+      (make-field "c1" 16 #u8(23 42))))
+
+  (test-format "symbolic input format"
+    #(#("s1" 8 #("Eq w8 s1 42")))
+    (make-input-format
+      (make-field "s1" 8 #("Eq w8 s1 42"))))
+
+  (test-format "mixed input format"
+    #(#("s1" 8 #("Eq w8 s1 5"))
+      #("c1" 16 #(#x23 #x42)))
+    (make-input-format
+      (make-field "s1" 8 #("Eq w8 s1 5"))
+      (make-field "c1" 16 #(#x23 #x42)))))
+
+;; Exit with non-zero exit status on test failure.
+(test-exit)
