4a6a020f261a1446215c5d73cde0e3d54a46ce2f
Sören Tempel <tempel@uni-bremen.de>
2022-01-27 23:17:11 +0100

    examples: Initialize

---
+++ examples/icmpv6.scm
@@ -0,0 +1,20 @@
+#!/usr/bin/csi -s
+
+(import (scheme base) sisl)
+
+(define ipv6-version-value #x6)
+(define icmpv6-next-header 58)
+
+(define ipv6-packet
+  (make-input-format
+    (make-uint "verison-field" 4 ipv6-version-value)
+    (make-uint "traffic-class" 8 #x0)
+    (make-uint "flow-label" 20 #x0)
+    (make-uint "payload-length" 16 4) ;; 4 bytes
+    (make-uint "next-header" 8 icmpv6-next-header)
+    (make-uint "hop-limit" 8 #x42)
+    (make-uint "src-addr" 128 #xffffffffffffffffffffffffffffffff)
+    (make-uint "dest-addr" 128 #xffffffffffffffffffffffffffffffff)
+    (make-uint "payload" 32 #xdeadbeef)))
+
+(write-format ipv6-packet)
