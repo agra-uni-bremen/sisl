482e812ca9fc41954613a23b43532931e0e4e2fa
Sören Tempel <tempel@uni-bremen.de>
2022-01-31 11:41:52 +0100

    examples: use loopback ipv6 address for destination

--- examples/icmpv6.scm
+++ examples/icmpv6.scm
@@ -3,6 +3,11 @@
 (import (scheme base) sisl)
 
 (define ipv6-version-value #x6)
+(define ipv6-loopback #u8(#x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x00
+                          #x00 #x00 #x00 #x01))
+
 (define icmpv6-next-header 58)
 
 (define-input-format (ipv6-packet next-hdr payload-field)
@@ -12,8 +17,8 @@
   (make-uint "payload-length" 16 (field-bytesize payload-field))
   (make-uint "next-header" 8 next-hdr)
   (make-uint "hop-limit" 8 #x42)
-  (make-uint "src-addr" 128 #xffffffffffffffffffffffffffffffff)
-  (make-uint "dest-addr" 128 #xffffffffffffffffffffffffffffffff)
+  (make-symbolic "src-addr" 128)
+  (make-concrete "dest-addr" 128 ipv6-loopback)
   payload-field)
 
 (write-format
